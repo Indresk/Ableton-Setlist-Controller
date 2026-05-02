6a9b3dc10d60d57f14947fd0e358e0a1acc8c9f8
Indresk <gif.poto@gmail.com>
2026-05-02 15:31:49 -0500

    fix origin crossdevices

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -2,6 +2,7 @@ import { createServer } from 'http';
 import app from './app.js';
 import { initSockets } from './sockets/index.js';
 import { initAbleton } from './services/ableton/boot.service.js';
+import { getIP } from './utils/getIP.js';
 
 const server = createServer(app);
 
@@ -11,5 +12,5 @@ initAbleton();
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on http://${getIP()}:${PORT}`);
 });
